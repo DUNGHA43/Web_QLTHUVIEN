15b39d4eb428f54786c1a1b59314bd6f3b856bc3
DUNGHA <dunghasonlla@outlook.com>
2023-12-23 12:14:02 +0700

    fix include

--- View/client/layouts/default.php
+++ View/client/layouts/default.php
@@ -8,11 +8,15 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   </head>
   <body>
-    <?php include("../partials/header.php"); ?>
+    <?php 
+        $htmlFilePath = CLIENT_PATH . "partials/header.php";
+        include $htmlFilePath; ?>
     <?php 
         echo $content;
     ?>
-    <?php include('../partials/footer.php'); ?>
+    <?php 
+        $htmlFilePath = CLIENT_PATH . "partials/footer.php";
+        include $htmlFilePath; ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
   </body>
 </html>
