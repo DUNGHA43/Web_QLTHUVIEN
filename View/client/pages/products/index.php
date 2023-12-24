acc0e13d5cbc3716d5e7257250560ec11a95db8e
hoanggggf <106527331+vuhoang001@users.noreply.github.com>
2023-12-24 15:34:34 +0700

    Build browser

--- View/client/pages/products/index.php
+++ View/client/pages/products/index.php
@@ -5,12 +5,65 @@ ob_start(); // Bắt đầu bộ nhớ đệm đầu ra
 ?>
 
 <div class="container-fluid">
-    <div class="container header">
-        <div class="row">
-            <div class="input-group mt-5">
+    <div class="container header d-flex justify-content-center">
+        <div class="row w-75 mt-5">
+            <div class="input-group ">
                 <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2">
                 <div class="input-group-append">
                     <button class="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container my-3">
+        <div class="row">
+            <div class="col-12 mb-4">
+                <h1>Books list</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-3">
+                <div class="card" style="width: 18rem;">
+                    <img src="public/image/lofi.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-3">
+                <div class="card" style="width: 18rem;">
+                    <img src="public/image/lofi.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-3">
+                <div class="card" style="width: 18rem;">
+                    <img src="public/image/lofi.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-3">
+                <div class="card" style="width: 18rem;">
+                    <img src="public/image/lofi.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
                 </div>
             </div>
         </div>
---
+++ View/client/partials/footer.php
@@ -0,0 +1,52 @@
+<footer class="text-black">
+    <div class="contaienr-fluid pt-5 ">
+        <div class="row bg-light">
+            <div class="col-md-2">
+                <h1 class="font-weight-bold mt-5 pt-2">
+                    Nike
+                </h1>
+            </div>
+
+            <div class="col-md-3">
+                <h4 class="font-weight-bold mb-3">
+                    Gift cards
+                </h4>
+                <p>Productions</p>
+                <p>Find & Store</p>
+                <p>Sign up mail</p>
+                <p>Book a member</p>
+                <p>Productions</p>
+                <p>Productions</p>
+            </div>
+            <div class="col-md-3">
+                <h4 class="font-weight-bold mb-3">
+                    Gift cards
+                </h4>
+                <p>Productions</p>
+                <p>Find & Store</p>
+                <p>Sign up mail</p>
+                <p>Book a member</p>
+                <p>Productions</p>
+                <p>Productions</p>
+            </div>
+
+            <div class="col-md-3">
+                <h4 class="font-weight-bold mb-3">
+                    Gift cards
+                </h4>
+                <p>Productions</p>
+                <p>Find & Store</p>
+                <p>Sign up mail</p>
+                <p>Book a member</p>
+                <p>Productions</p>
+                <p>Productions</p>
+            </div>
+
+            <div class="col-md-1">
+                <img src="public/image/lofi.jpg" width="80px" alt="">
+            </div>
+        </div>
+
+
+    </div>
+</footer>
