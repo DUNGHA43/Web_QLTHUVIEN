59eef39231e545488872634ecc7610aae066126b
hoanggggf <106527331+vuhoang001@users.noreply.github.com>
2023-12-26 00:20:21 +0700

    first commit

--- View/client/pages/products/index.php
+++ View/client/pages/products/index.php
@@ -24,11 +24,11 @@ ob_start(); // Bắt đầu bộ nhớ đệm đầu ra
         </div>
         <div class="row">
             <div class="col-3">
-                <div class="card" style="width: 18rem;">
+                <div class="card" style="width: 18rem; ">
                     <img src="public/image/lofi.jpg" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <p class="card-text">Lorem ipsum dolor, sit amet consectint eos nostrum quas unde qui esse illum aut. Debitis harum, et voluptate aliquam saepe molestias, possimus mollitia consectetur, quod eos eum ut? Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                         <a href="#" class="btn btn-primary">Go somewhere</a>
                     </div>
                 </div>
--- index.php
+++ index.php
@@ -4,6 +4,6 @@ define('ADMIN_PATH', __DIR__ . '/View/admin/');
 define('CSS_PATH', __DIR__ . '/public/css/');
 $htmlFilePath = CLIENT_PATH . 'pages/products/index.php';
 $dashboard_admin_path = ADMIN_PATH . "pages/dashboard/index.php";
-// include $htmlFilePath;
-include $dashboard_admin_path;
+include $htmlFilePath;
+// include $dashboard_admin_path;
 
