fcfdd39abcd9f72c2eb6a97011dec02a1d818485
hoanggggf <106527331+vuhoang001@users.noreply.github.com>
2023-12-25 10:20:49 +0700

    fix sensitive

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 define('CLIENT_PATH', __DIR__ . '/View/client/');
-define('ADMIN_PATH', __DIR__, 'View/admin/');
+define('ADMIN_PATH', __DIR__ . 'View/admin/');
 define('CSS_PATH', __DIR__ . '/public/css/');
 $htmlFilePath = CLIENT_PATH . 'pages/products/index.php';
 
