54d053bac31a0f1e34d322824ded0f1d1d9941bf
DUNGHA <dunghasonlla@outlook.com>
2023-12-27 16:07:33 +0700

    t

--- Controller/account_Controller.php
+++ Controller/account_Controller.php
@@ -16,5 +16,5 @@
 
         addAccount($taiKhoan, $matKhau, $maSV, $hoTen, $ngaySinh, $soCCCD,$soDT, $email, $diaChi, $maquyen, $gioiTinh);
     }
-    header("location: index.php");
+    header("location: ./index.php");
 ?>
