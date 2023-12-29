1a83343df4cc122aa09ea9c0ad0744621488a01d
trunghieu2803 <20213613@eaut.edu.vn>
2023-12-29 08:54:44 +0700

    sua may file ve dang nhap dang ki

--- Model/account_Model.php
+++ Model/account_Model.php
@@ -1,10 +1,25 @@
 <?php 
+
     include "../Model/connect.php";
+
+    function checkUser($user, $pass){
+        $check = "";
+        $conn = connectSQLPDO();
+        $stmt = $conn->prepare("SELECT * FROM tbltaikhoan WHERE taiKhoan = '".$user."' and matKhau = '".$pass."'");
+        $stmt -> execute();
+        $rs = $stmt -> setFetchMode(PDO::FETCH_ASSOC);
+        $kq = $stmt -> fetchAll();
+        if ($rs != null) {
+            $check = $kq[0]['maQuyen'];
+        }
+        return $check;
+    }
+
     function addAccount($taiKhoan, $matKhau, $maSV, $hoTen, $ngaySinh, $soCCCD, $soDT, $email, $gioiTinh, $diaChi, $maquyen){
         $conn = connectSQL();
         $sql = "INSERT INTO tbltaikhoan VALUES ('$taiKhoan', '$matKhau', '$maSV', '$hoTen', '$ngaySinh',
                                                                 '$soCCCD','$soDT', '$email', '$gioiTinh', '$diaChi', '$maquyen')";
-        $result = mysqli_query($conn,$sql);
-        mysqli_close($conn);
+        mysqli_query($conn,$sql);
+        $conn = null;
     }
 ?>
--- View/client/pages/products/index.php
+++ View/client/pages/products/index.php
@@ -23,58 +23,10 @@ ob_start(); // Bắt đầu bộ nhớ đệm đầu ra
                 <h1>Ranking board</h1>
             </div>
         </div>
-        <div class="row">
-            <div class="col-3">
-                <div class="card" style="width: 18rem;">
-                    <img src="public/image/lofi.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-3">
-                <div class="card" style="width: 18rem;">
-                    <img src="public/image/lofi.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-3">
-                <div class="card" style="width: 18rem;">
-                    <img src="public/image/lofi.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-3">
-                <div class="card" style="width: 18rem;">
-                    <img src="public/image/lofi.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-
-    <div class="container my-3">
+        
         <div class="row">
             <?php 
-                while($rows = mysqli_fetch_array($result)){        
+                while($rows = $result->fetch_assoc()){        
             ?>
             <div class="col-3" style="padding-top: 20px;">
                 <div class="card" style="width: 18rem;">
