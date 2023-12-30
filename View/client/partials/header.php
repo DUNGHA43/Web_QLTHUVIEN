9acfbadcba84669699c007a732a2404687dcd51c
DUNGHA <dunghasonlla@outlook.com>
2023-12-31 01:55:25 +0700

    add menu

--- View/client/partials/header.php
+++ View/client/partials/header.php
@@ -13,10 +13,10 @@
     <?php if (isset($_SESSION['hoTen']) && isset($_SESSION['img']) && ($_SESSION['hoTen'] != "")) {
       echo '<div class="dropdown ">
       <button class="btn btn-secondary dropdown-toggle buttonDropdown" type="button" data-toggle="dropdown" aria-expanded="false">
-        
+      <span class= "mr-2 d-none d-lg-inline text-gray-600 small">'. $_SESSION['hoTen'] . '</span>
+      <img src="public/client/image/'.$_SESSION['img'].'" alt="" class="img-profile rounded-circle"">
       </button>
       <div class="dropdown-menu">
-        
         <button class="dropdown-item" type="button">Action</button>
         <button class="dropdown-item" type="button">Another action</button>
         <button class="dropdown-item" type="button">Something else here</button>
@@ -24,18 +24,17 @@
     </div>';
     } else {
     ?>
-
+      <span class= "mr-2 d-none d-lg-inline text-gray-600 small"><?php $_SESSION['hoTen'] ?></span>
       <a class="navbar-brand" href="http://localhost/Web_QLTHUVIEN/View/client/pages/products/dangki.php">Đăng ký</a>
       <a class="navbar-brand" href="http://localhost/Web_QLTHUVIEN/View/client/pages/products/dangnhap.php">Đăng nhập</a>
     <?php } ?>
   </form>
 </nav>
-
+<a href=""></a>
 <style>
   .form-infor {
     padding-left: 10px;
   }
-
 
   .form-infor a {
     padding-left: 30px;
@@ -47,16 +46,18 @@
   }
 
   .img-profile {
-    width: 45px;
-    height: 45px;
+    width: 30px;
+    height: 30px;
   }
 
   .buttonDropdown {
-    background-color: red;
-    width: 40px;
+    background-color: cornflowerblue;
+    margin-right: 10px;
+    width: 180px;
+    /* width: 40px;
     height: 40px;
     border-radius: 50%;
-    margin-right: 70px;
+    margin-right: 70px; */
   }
 
 
