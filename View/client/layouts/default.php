7faa5b5424b6706017b95733e6cfc995cb61e752
hoanggggf <106527331+vuhoang001@users.noreply.github.com>
2023-12-31 01:00:48 +0700

    Chỉnh dropdown

--- View/client/layouts/default.php
+++ View/client/layouts/default.php
@@ -1,31 +1,37 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?php echo $pageTitle ?></title>
-    <link rel="stylesheet" href="public/client/css/style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <style>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title><?php echo $pageTitle ?></title>
+  <link rel="stylesheet" href="public/client/css/style.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <style>
     .text-ellipsis {
       overflow: hidden;
       white-space: nowrap;
       text-overflow: ellipsis;
       max-width: 200px;
-      cursor: pointer; /* Thêm con trỏ để chỉ ra tính tương tác */
+      cursor: pointer;
+      /* Thêm con trỏ để chỉ ra tính tương tác */
     }
   </style>
-  </head>
-  <body>
-    
-    <?php 
-        $header_path = CLIENT_PATH . "partials/header.php";
-        include $header_path;
-        echo $content;
-    ?>
-    <?php 
-        $htmlFilePath = CLIENT_PATH . "partials/footer.php";
-        include $htmlFilePath; ?>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-  </body>
+</head>
+
+<body>
+
+  <?php
+  $header_path = CLIENT_PATH . "partials/header.php";
+  include $header_path;
+  echo $content;
+  ?>
+  <?php
+  $htmlFilePath = CLIENT_PATH . "partials/footer.php";
+  include $htmlFilePath; ?>
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+</body>
+
 </html>
