df42bcef224762e5d6234b28a4c420ffb12f0e35
DUNGHA <dunghasonlla@outlook.com>
2024-01-15 09:39:45 +0700

    .

--- View/admin/pages/products/index.php
+++ View/admin/pages/products/index.php
@@ -6,12 +6,7 @@ include "../Web_QLTHUVIEN/Model/CRUD_Model.php";
 ?>
 
 
-<form method="post" enctype="multipart/form-data" action="http://localhost/Web_QLTHUVIEN/config/process-form.php">
-    <!-- <input type="hidden" name="MAX_FILE_SIZE" value="1048576"> -->
-    <label for="image">Image file</label>
-    <input type="file" id="image" name="image">
-    <button>Upload</button>
-</form>
+
 <h1><span class="badge badge-secondary mb-5">Sửa tên ở đây nè!</span></h1>
 <div class="container-fluid mb-5">
     <div class="row">
