e426949432985f5757e5004aec767f4e3a90b332
DUNGHA <dunghasonlla@outlook.com>
2024-01-16 19:29:14 +0700

    change color hd md ft

--- View/client/pages/products/index.php
+++ View/client/pages/products/index.php
@@ -5,13 +5,13 @@ include $dataFile;
 $pageTitle = "Page Title";
 ob_start(); // Bắt đầu bộ nhớ đệm đầu ra
 ?>
-<div class="container-fluid bg-light.bg-gradient">
+<div class="container-fluid bg-light bg-gradient">
     <div class="container header d-flex justify-content-center">
         <div class="row w-75 mt-5">
             <div class="input-group" style="margin-top: 5%;">
-                <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2">
-                <div class="input-group-append">
-                    <button class="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
+                <input type="text" class="form-control" placeholder="Nhập tên tài liệu ..." aria-label="Recipient's username" aria-describedby="button-addon2">
+                <div class="input-group-append" style="margin-left: 1%;">
+                    <button class="btn btn-outline-secondary" type="button" id="button-addon2">Tìm kiếm</button>
                 </div>
             </div>
         </div>
